daaeda10dc4f797ba228d640ac8fcfca4b7fcdc3
HarySteeve <grandturismo2005@gmail.com>
2025-11-21 07:25:33 +0300

    implementation sprint-6
    Ajout du fonction getMatchedParamsName qui injecte les valeur dans un vue dans dans les arguments d'une fonction dont les noms match

--- src/main/java/util/http/ResponseHandler.java
+++ src/main/java/util/http/ResponseHandler.java
@@ -4,7 +4,11 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.Arrays;
 import java.util.Map;
+
+import org.apache.commons.beanutils.ConvertUtils;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletContext;
@@ -48,13 +52,14 @@ public class ResponseHandler {
             Method m = cm.method;
             m.setAccessible(true);
 
+            Object[] args = getMatchedParams(m, req);
             Class<?> returnType = m.getReturnType();
             Object objectController = c.getDeclaredConstructor().newInstance();
             if(returnType.equals(String.class)) {
                 res.setContentType("text/plain");
-                responseBody = m.invoke(objectController).toString();
+                responseBody = m.invoke(objectController, args).toString();
             } else if(returnType.equals(ModelAndView.class)){
-                ModelAndView mv = (ModelAndView)m.invoke(objectController);
+                ModelAndView mv = (ModelAndView)m.invoke(objectController, args);
                 Map<String, Object> data = mv.getData();
                 if(data != null) {
                     for (Map.Entry<String, Object> entry : data.entrySet()) {
@@ -73,6 +78,32 @@ public class ResponseHandler {
         } catch (ServletException | IOException ex) { // From requestDispatcher.forward()
             handleError(res, "Error forwarding to view: " + ex.getMessage());
         }
+    }
+
+private Object[] getMatchedParams(Method method, HttpServletRequest req) {
+        Parameter[] parameters = method.getParameters();
+        Object[] args = new Object[parameters.length];
+
+        Map<String, String[]> paramsViaVue = req.getParameterMap();
+        paramsViaVue.forEach((key, value) ->
+            System.out.println(key + " => " + Arrays.toString(value))
+        );
+        for (int i = 0; i < parameters.length; i++) {
+            Parameter p = parameters[i];
+            String paramName = p.getName();
+            System.out.println("PARAMNAME---- "+paramName);
+
+            if(paramsViaVue.containsKey(paramName)) {
+                String value = paramsViaVue.get(paramName)[0];
+                Class<?> typeArg = p.getType();
+
+                Object convertedValue = ConvertUtils.convert(value, typeArg);
+                args[i] = convertedValue;
+            } else {
+                args[i] = null;
+            }
+        }
+        return args;
     }
 
     private void handleError(HttpServletResponse res, String error) {
